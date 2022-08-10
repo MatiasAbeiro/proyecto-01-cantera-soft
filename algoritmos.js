a7b1b9f65f2aae66cd2fd26872da94f28eefcfa8
Matias Abeiro <matute0721@gmail.com>
2022-08-10 17:27:01 -0300

    este es commit del proyecto algoritmo.js

---
+++ algoritmos.js
@@ -0,0 +1,49 @@
+/* function suma () {
+var a = 4 ;
+var b = 2 ;
+
+var resultado = a+b ;
+
+if ( console > 3){
+    return console.log ("el resultado es mayor a 3"); } else {
+    return console.log ("el resultado es menor a 3");
+    }
+}
+suma();  */
+/* function resta () {
+var a = 10 ;
+var b = 5 ;
+
+var resultado = a - b ;
+if (resultado > 5){
+    return console.log ("el resultado es mayor a 4");
+
+    }else { 
+return console.log (" el resultado es menor a 5")
+    }
+}
+resta() ; */
+/* function multiplicacion () {
+var a = 5;
+var b = 8;
+var resultado = a * b ;
+if (resultado > 30){
+    return console.log ("el resultado en mayor a 30");
+}
+else return console.log (" el resultado es menor a 50") ;
+}
+multiplicacion ();  */
+
+
+function division() {
+var a = 4 ;
+var b = 2 ;    
+var resultado = a / b ;
+if (resultado >3)
+{return console.log ("el resultado es mayor a 3");
+    
+} else { console.log ("el resultado es menor a 2");
+    
+}
+}
+division ()
